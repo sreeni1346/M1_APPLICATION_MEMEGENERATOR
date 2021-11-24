a83bece3a92bc48b34c93d608c3dfd5f3f06b59b
sreeni1346 <94226412+sreeni1346@users.noreply.github.com>
2021-11-24 16:27:51 +0530

    Update tour.h

--- 3_IMPLEMENTATION/INC/tour.h
+++ 3_IMPLEMENTATION/INC/tour.h
@@ -47,7 +47,7 @@ userprofile* InitializeListing(userprofile *hello)
         return 0;
 
     rewind(fileopen);
-	while(fscanf(fileopen,"%s20 %s20 %s20 %f20 %d",temporary.user_fname,temporary.user_password,temporary.user_place,&temporary.ticket_price,&temporary.user_numtick)!=EOF)
+	while(fscanf(fileopen,"%90s %90s %90s %90f %90d",temporary.user_fname,temporary.user_password,temporary.user_place,&temporary.ticket_price,&temporary.user_numtick)!=EOF)
 	{
 		petrol=(userprofile*)malloc(sizeof(userprofile));
 		strcpy(petrol->user_fname,temporary.user_fname);
@@ -156,10 +156,10 @@ void login_client(userprofile* hello)
     fflush(stdin);
     printf("\n\n");
     printf("\t\tEnter Email/Username:\n\t\t");
-    scanf("%s100",client_username);
+    scanf("%90s",client_username);
     fflush(stdin);
     printf("\n\t\tEnter Password:\n\t\t");
-    scanf(" %s100[^\n]",client_password);
+    scanf(" %90s[^\n]",client_password);
     while(hello!=NULL)
     {
         if((!strcmp(hello->user_fname,client_username)) && (!strcmp(hello->user_password,client_password)))
@@ -202,11 +202,11 @@ void booking_ticket(userprofile *hello)
     float pricelist[]={3000.0,5000.0,2000.0,10000.0,12000.0,5000.0,7000.0,8000.0,12000.0,14000.0};
     fflush(stdin);
     printf("\nEnter place code (eg: AA, BB)\n");
-    scanf(" %[^\n]s100",place);
+    scanf(" %[^\n]100s",place);
     char choice;
     fflush(stdin);
     printf("\nWould You Like to Confirm Booking?\n[1] - Yes\n[2] - No\n");
-    scanf("%s5",&choice);
+    scanf("%5s",&choice);
     float price;
     if(choice!='1')
         return;
@@ -336,7 +336,7 @@ void changing_pass(userprofile *hello)
     char passcurr[100];
     fflush(stdin);
     printf("Enter your current password to continue:\n");
-    scanf(" %[^\n]s100",passcurr);
+    scanf(" %[^\n]100s",passcurr);
     while(hello!=NULL)
     {
         if(!strcmp(hello->user_fname,current_client))
@@ -348,7 +348,7 @@ void changing_pass(userprofile *hello)
     if(!strcmp(passcurr,hello->user_password))
     {
         printf("Enter new password:\n");
-        scanf(" %[^\n]s100",hello->user_password);
+        scanf(" %[^\n]100s",hello->user_password);
     }
     WriteToFile(trans);
 }
